e90ddfb9be1573eb47099c396228e9a7a14efb1a
Malachi Spencer <malachispencer@MA309.local>
2021-01-15 11:54:10 +0000

    Refactored User tests for cleaner code

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,7 +1,11 @@
 describe User do
   describe '.create' do
     it 'wraps user info in a User object' do
-      user = User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: 'p20201124')
+      user = User.create(
+        name: 'Malachi', 
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
 
       expect(user.name).to eq('Malachi')
       expect(user.email).to eq('m.spencer@makers.com')
@@ -10,13 +14,16 @@ describe User do
 
   describe '.find' do
     it 'returns nil if id is given as nil' do
-      User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: 'p20201124')
-
       expect(User.find(id: nil)).to be_nil
     end
 
     it 'retrieves user from db by id' do
-      user = User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: 'p20201124')
+      user = User.create(
+        name: 'Malachi', 
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
+
       found_user = User.find(id: user.id)
 
       expect(found_user.id).to eq(user.id)
@@ -26,37 +33,65 @@ describe User do
 
   describe '.authenticate' do
     it 'returns the user given a correct email and password' do
-      user = User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: 'p20201124')
-      authenticated_user = User.authenticate(email: 'm.spencer@makers.com', password: 'p20201124')
+      user = User.create(
+        name: 'Malachi', 
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
 
-      expect(authenticated_user.id).to eq user.id
-      expect(authenticated_user.email).to eq user.email
+      authenticated_user = User.authenticate(
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
+
+      expect(authenticated_user.id).to eq(user.id)
+      expect(authenticated_user.email).to eq(user.email)
     end
 
     it 'retusn nil if user enters wrong email' do
-      User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: 'p20201124')
-      authenticated_user = User.authenticate(email: 'a.spencer@makers.com', password: 'p20201124')
+      User.create(
+        name: 'Malachi', 
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
 
-      expect(authenticated_user).to eq nil
+      authenticated_user = User.authenticate(
+        email: 'm.spencer@codeworks.com', 
+        password: '2020'
+      )
+
+      expect(authenticated_user).to be_nil
     end
 
     it 'retusn nil if user enters wrong password' do
-      User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: 'p20201124')
-      authenticated_user = User.authenticate(email: 'm.spencer@makers.com', password: 'a20201124')
+      User.create(
+        name: 'Malachi', 
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
 
-      expect(authenticated_user).to eq nil
+      authenticated_user = User.authenticate(
+        email: 'm.spencer@makers.com', 
+        password: 'wrong_password'
+      )
+
+      expect(authenticated_user).to be_nil
     end
   end
 
   describe '.find_by_email' do
     it 'returns nil if no user with this email exists' do
-      found_user = User.find_by_email(email: 'm.spencer@makers.com')
+      found_user = User.find_by_email(email: 'm.spencer@codeworks.com')
 
       expect(found_user).to be_nil
     end
 
     it 'returns user if user found' do
-      User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: '2020')
+      User.create(
+        name: 'Malachi', 
+        email: 'm.spencer@makers.com', 
+        password: '2020'
+      )
 
       found_user = User.find_by_email(email: 'm.spencer@makers.com')
 

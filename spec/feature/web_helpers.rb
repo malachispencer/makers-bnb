fc9a6c3d3ea29d6c4383c11e338ecb3f2062293b
Malachi Spencer <malachispencer@MA309.local>
2021-01-17 20:30:32 +0000

    Renamed features folder to feature

--- spec/feature/web_helpers.rb
+++ spec/feature/web_helpers.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
-
 def login_to_dashboard
   User.create(name: 'Malachi', email: 'm.spencer@makers.com', password: '2020')
-  visit('/')
-  click_link('Login')
+  visit('/sessions/new')
   fill_in('email', with: 'm.spencer@makers.com')
   fill_in('password', with: '2020')
   click_button('Login')
@@ -19,16 +16,14 @@ def make_request
     user_id: host.id
   )
   login_to_dashboard
-  fill_in('check_in_date', with: '07/17/2019 12:00 AM')
+  fill_in('check_in_date', with: '07/17/2019')
   click_button('Search Properties')
-  expect(current_path).to eq('/listings')
   first('.card').click_button('Request Space')
   click_link('dashboard')
 end
 
 def login_as_host
-  visit('/')
-  click_link('Login')
+  visit('/sessions/new')
   fill_in('email', with: 'constantine@makers.com')
   fill_in('password', with: '12345qwerty')
   click_button('Login')

3727a4f5192e9de126a7bd6232068515b382197d
Malachi Spencer <malachispencer@MA309.local>
2021-01-17 10:33:36 +0000

    Major refactoring and cleaning up of Booking tests

--- spec/unit/booking_spec.rb
+++ spec/unit/booking_spec.rb
@@ -1,62 +1,93 @@
 describe Booking do
-  let(:test_user) { 
+  let(:host) { 
     User.create(
-      name: 'Jane Doe', 
-      email: 'jane_doe@gmail.com', 
-      password: '12345qwerty'
-    ) 
+      name: 'Malachi',
+      email: 'm.spencer@makers.com',
+      password: '2020'
+    )
+  }
+
+  let(:guest) { 
+    User.create(
+      name: 'Constantine',
+      email: 'constantine@makers.com',
+      password: '2020'
+    )
+  }
+
+  let(:guest_two) {
+    User.create(
+      name: 'Alex',
+      email: 'alm@makers.com',
+      password: '2020'
+    )
   }
 
   let(:space) {
     Space.create(
-      name: 'Hidden Gem of Beverly Hills',
-      description: 'A luxurious villa in Beverly Hills',
-      location: 'Los Angeles, Beverly Hills', 
-      price: 300, 
-      user_id: test_user.id
+      name: 'Ealing Flat',
+      description: 'Studio apartment',
+      location: 'Ealing, London, UK',
+      price: 150,
+      user_id: host.id
     )
   }
 
-  context '.create' do
-    it 'is called on the Booking class' do
-      expect(described_class).to respond_to(:create).with_keywords(:check_in, :booked, :space_id, :user_id)
-    end
+  let(:today) { Date.today.to_s }
 
-    it 'creates a new booking' do
-      booking = described_class.create(check_in: Date.today.to_s, booked: false,
-                                       space_id: space.id, user_id: test_user.id)
-      expect(booking.check_in).to eq(Date.today.to_s)
+  describe '.create' do
+    it 'adds a booking to the database and returns it' do
+      booking = Booking.create(
+        check_in: today,
+        booked: false,
+        space_id: space.id,
+        user_id: guest.id
+      )
+
+      expect(booking.check_in).to eq(today)
       expect(booking.booked).to eq('f')
       expect(booking.space_id).to eq(space.id)
-      expect(booking.user_id).to eq(test_user.id)
+      expect(booking.user_id).to eq(guest.id)
     end
   end
 
-  context '.retrieve_booking' do
-    it 'is called on the Booking class' do
-      expect(described_class).to respond_to(:retrieve_booking)
-    end
+  describe '.all' do
+    it 'gets all bookings from the bookings table' do
+      booking_one = Booking.create(
+        check_in: today,
+        booked: false,
+        space_id: space.id,
+        user_id: guest.id
+      )
 
-    it 'retrieves all bookings from the bookings table' do
-      described_class.create(check_in: Date.today.to_s, booked: false,
-                             space_id: space.id, user_id: test_user.id)
-      result = described_class.retrieve_booking
-      expect(result.first.check_in).to eq(Date.today.to_s)
-      expect(result.first.booked).to eq('f')
-      expect(result.first.space_id).to eq(space.id)
-      expect(result.first.user_id).to eq(test_user.id)
+      booking_two = Booking.create(
+        check_in: today,
+        booked: false,
+        space_id: space.id,
+        user_id: guest_two.id
+      )
+
+      results = Booking.all
+
+
+      expect(results.length).to eq(2)
     end
   end
 
   describe '.retrieve_requests_made' do
     it 'retrieves all requests made by a specific user' do
-      described_class.create(check_in: Date.today.to_s, booked: false,
-                             space_id: space.id, user_id: test_user.id)
-      request_made = described_class.retrieve_requests_made(user_id: test_user.id).first
+      booking = Booking.create(
+        check_in: today,
+        booked: false,
+        space_id: space.id,
+        user_id: guest.id
+      )
 
-      expect(request_made['host_name']).to eq(test_user.name)
-      expect(request_made['check_in']).to eq(Date.today.to_s)
-      expect(request_made['name']).to eq('Hidden Gem of Beverly Hills')
+      requests_made = Booking.retrieve_requests_made(user_id: guest.id).first
+
+      expect(requests_made['host_name']).to eq(host.name)
+      expect(requests_made['check_in']).to eq(today)
+      expect(requests_made['name']).to eq(space.name)
     end
   end
 
@@ -66,40 +97,49 @@ describe Booking do
         check_in: Date.today.to_s, 
         booked: false,
         space_id: space.id, 
-        user_id: test_user.id
+        user_id: guest.id
       )
 
-      result = Booking.retrieve_requests_received(user_id: space.user_id).first
+      result = Booking.retrieve_requests_received(user_id: host.id).first
 
-      expect(result['guest_name']).to eq(test_user.name)
-      expect(result['name']).to eq('Hidden Gem of Beverly Hills')
+      expect(result['guest_name']).to eq(guest.name)
+      expect(result['name']).to eq(space.name)
     end
   end
 
   describe '.confirm' do
-    it 'is called on the Booking class' do
-      expect(described_class).to respond_to(:confirm).with_keywords(:booking_id)
-    end
+    it 'updates booked to TRUE' do
+      booking = Booking.create(
+        check_in: Date.today.to_s, 
+        booked: false,
+        space_id: space.id, 
+        user_id: guest.id
+      )
 
-    it 'updates the booked column so that it equals TRUE for the given booking_id' do
-      result = described_class.create(check_in: Date.today.to_s, booked: false,
-                                      space_id: space.id, user_id: test_user.id)
-      confirmation = Booking.confirm(booking_id: result.id)
+      confirmation = Booking.confirm(booking_id: booking.id)
+
       expect(confirmation.first['booked']).to eq('t')
     end
   end
 
   describe '.deny' do
-    it 'is called on the Booking class' do
-      expect(described_class).to respond_to(:deny).with_keywords(:booking_id)
-    end
+    it 'removes the booking from the bookings table' do
+      booking = Booking.create(
+        check_in: Date.today.to_s, 
+        booked: false,
+        space_id: space.id, 
+        user_id: guest.id
+      )
 
-    it 'removes the booking entry from the bookings table' do
-      result = described_class.create(check_in: Date.today.to_s, booked: false,
-                                      space_id: space.id, user_id: test_user.id)
-      Booking.deny(booking_id: result.id)
-      verdict = DatabaseConnection.query("SELECT * FROM bookings WHERE id='#{result.id}';")
-      expect(verdict.first).to be_nil
+      Booking.deny(booking_id: booking.id)
+
+      db_response = DatabaseConnection.query(
+        "SELECT * 
+        FROM bookings 
+        WHERE id = '#{booking.id}';"
+      ).first
+
+      expect(db_response).to be_nil
     end
   end
 
@@ -109,16 +149,16 @@ describe Booking do
         check_in: Date.today.to_s, 
         booked: true,
         space_id: space.id, 
-        user_id: test_user.id
+        user_id: guest.id
       )
 
-      result = Booking.retrieve_confirmed_bookings(
+      results = Booking.retrieve_confirmed_bookings(
         host_or_guest: 'guest', 
-        user_id: test_user.id
+        user_id: guest.id
       ).first
 
-      expect(result['host_name']).to eq(test_user.name)
-      expect(result['host_email']).to eq(test_user.email)
+      expect(results['host_name']).to eq(host.name)
+      expect(results['host_email']).to eq(host.email)
     end
 
     it 'retrieves all confirmed bookings as host' do
@@ -126,16 +166,16 @@ describe Booking do
         check_in: Date.today.to_s, 
         booked: true,
         space_id: space.id, 
-        user_id: test_user.id
+        user_id: guest.id
       )
 
       result = Booking.retrieve_confirmed_bookings(
         host_or_guest: 'host', 
-        user_id: test_user.id
+        user_id: host.id
       ).first
 
-      expect(result['guest_name']).to eq(test_user.name)
-      expect(result['guest_email']).to eq(test_user.email)
+      expect(result['guest_name']).to eq(guest.name)
+      expect(result['guest_email']).to eq(guest.email)
     end
   end
 end

e7656b1348ab28a031b978add01a42b67381d4ce
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-02-01 15:48:24 +0000

    Commented out database connection

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -1,7 +1,7 @@
-require_relative './lib/database_connection'
+# require_relative './lib/database_connection'
 
-if ENV['ENVIRONMENT'] == 'test'
-  DatabaseConnection.setup(dbname: 'makers_bnb_test')
-else
-  DatabaseConnection.setup(dbname: 'makers_bnb')
-end
+# if ENV['ENVIRONMENT'] == 'test'
+#   DatabaseConnection.setup(dbname: 'makers_bnb_test')
+# else
+#   DatabaseConnection.setup(dbname: 'makers_bnb')
+# end

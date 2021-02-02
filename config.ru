cbc0d037f5be33f651539c221a31ea1c25580b84
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-02-02 13:18:12 +0000

    Changed ruby version and db connection

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+p 'IN CONFIG.RU >>>>>>>>>>>>>>>>>>>'
+
 require_relative './controllers/app'
 
 run MakersBnB
--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -2,6 +2,6 @@ require_relative './lib/database_connection'
 
 if ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup(dbname: 'makers_bnb_test')
-elsif ENV['ENVIRONMENT'] == 'development'
+else
   DatabaseConnection.setup(dbname: 'makers_bnb')
 end

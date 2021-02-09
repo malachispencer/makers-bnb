e351f79476f9b9b181a2a2f6867f67117b0a0143
Malachi Spencer <m.spencer@live.co.uk>
2021-02-09 14:35:09 +0000

    Using RACK_ENV to check if app running in production

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -2,7 +2,7 @@ require_relative './lib/database_connection'
 
 if ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup(dbname: 'makers_bnb_test')
-elsif ENV['ENVIRONMENT'] == 'production'
+elsif ENV['RACK_ENV'] == 'production'
   DatabaseConnection.production_setup
 else
   DatabaseConnection.setup(dbname: 'makers_bnb')
